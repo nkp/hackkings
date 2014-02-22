b37fce651ee4895c775b0d14c40e15f5a4433ff2
nkp <nicprettejohn@gmail.com>
2014-02-22 19:10:18 +0000

    Add db import to role.

--- models/role.py
+++ models/role.py
@@ -1,9 +1,9 @@
+from hackkings import db
 class Role(db.Model):
     __tablename__ = "role"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), unique=True)
-    users = db.relationship('User', backref='role',
-                                lazy='dynamic')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __init__(self, title):
         self.title = title
