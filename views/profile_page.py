fcadcbf8c0083ae2a2a8577dc7e4045f50c791e2
Ilija Radosavovic <ilija.radosavovic13@imperial.ac.uk>
2014-02-22 22:59:28 +0000

    Profile page

---
+++ views/profile_page.py
@@ -0,0 +1,33 @@
+from flask import render_template, current_app, abort
+from hackkings import app, db
+from hackkings.models import User
+from hackkings.constants import ROLES
+
+@app.route('/user/<id:int>')
+def profile_page(id=None):
+    if id == None:
+        abort(404)
+    user = User.find(id)
+    if user == None:
+        abort(404)
+
+    profile_data = {}
+    if user.role == DEVELOPER
+        profile_data = { 'ongoing_projects': user.get_ongoing_projects(),
+                         'completed_projects': user.get_completed_projects(),
+                         'username': user.username,
+                         'avatar': user.avatar,
+                         'description': user.bio,
+                         'skills': user.get_skills(),
+                         'codeacademy_badges': [] } 
+    elif user.role == PROPOSER
+        profile_data = { 'ongoing_proposals': user.get_ongoing_proposals(),
+                         'pending_proposals': user.get_pending_proposals(),
+                         'completed_proposals': user.get_completed_proposals(),
+                         'name': user.name,
+                         'description': user.bio,
+                         'avatar': user.avatar }
+    else
+        abort(400)
+
+    return render_template('profile.html', profile_data)
