702b63bfe581aa626bd8e73310ebf0ceafa3b6f9
Sachin Pande <sazap10@gmail.com>
2014-02-22 23:57:24 +0000

    removed completed field from project

--- models/project.py
+++ models/project.py
@@ -8,7 +8,6 @@ class Project(db.Model):
     __tablename__ = "project"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
-    completed = db.Column(db.Boolean, unique=False)
     #developers is back reffed
     state = db.Column(db.Integer, unique = False)
     proposer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
@@ -19,9 +18,8 @@ class Project(db.Model):
     difficulty = db.Column(db.Integer, unique=False)
     attachments = db.relationship('Attachment', backref='project', lazy='dynamic')
 
-    def __init__(self, name,completed,state,proposer,description,time_estimate,difficulty):
+    def __init__(self, name,state,proposer,description,time_estimate,difficulty):
         self.name = name
-        self.completed = completed
         self.state = state
         self.proposer = proposer
         self.description = description
