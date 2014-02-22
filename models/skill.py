193e5a7bbe969440672142bba4d8f273d898d6cd
Sachin Pande <sazap10@gmail.com>
2014-02-22 17:52:49 +0000

    modified skill class to add many to many relationship

--- models/skill.py
+++ models/skill.py
@@ -1,9 +1,13 @@
+from hackkings.models.project import Project
+from hackkings.models.user import User
+from hackkings.linkingtables import skill_projects_link, skill_users_link
+
 class Skill(db.Model):
     __tablename__ = "skill"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
-    users = db.relationship('User', backref='skill', lazy='dynamic')
-    projects = db.relationship('Project', backref='skill', lazy='dynamic')
+    projects = db.relationship('Project', secondary=skill_projects_link, backref=db.backref('skills', lazy='dynamic'))
+    users = db.relationship('User', secondary=skill_users_link, backref=db.backref('skills', lazy='dynamic'))
 
     def __init__(self, name):
         self.name = name
