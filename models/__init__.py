84bc18217cd8e94b4de6b0bd9f111d9cf3e65f07
Ilija Radosavovic <ilija.radosavovic13@imperial.ac.uk>
2014-02-22 18:44:16 +0000

    ...

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from hackkings.models.role import Role
 from hackkings.models.skill import Skill
 from hackkings.models.state import State
 from hackkings.models.message import Message
+from hackkings.models.attachment import Attachment
