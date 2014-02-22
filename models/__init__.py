e26eb13ab250f3ef41a40c940237f62e00a8ae35
Sachin Pande <sazap10@gmail.com>
2014-02-22 19:47:04 +0000

    removed state and role imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 from hackkings.models.user import User
-from hackkings.models.role import Role
 from hackkings.models.skill import Skill
-from hackkings.models.state import State
 from hackkings.models.message_thread import MessageThread
 from hackkings.models.message import Message
 from hackkings.models.project import Project
