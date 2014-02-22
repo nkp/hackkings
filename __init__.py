8c3a91a89ded29b2b289e78e959dbbf0894146a1
nkp <nicprettejohn@gmail.com>
2014-02-22 18:14:46 +0000

    Include SITENAME in all templates

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from hackkings.constants import SITENAME
 
 def configure(app):
     app.debug = True
@@ -8,6 +9,11 @@ def configure(app):
 
 def configuredb(app):
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
+def configure_jinja(app):
+    @app.context_processor
+    def inject_sitename():
+        return {'SITENAME':SITENAME}
 
 def hook_routes():
     from hackkings import views
