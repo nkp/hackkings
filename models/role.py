ef228d9efe3c74b39ffe9e980df696919cb571e0
Sachin Pande <sazap10@gmail.com>
2014-02-22 16:25:42 +0000

    added users relationship to role class

--- models/role.py
+++ models/role.py
@@ -2,7 +2,8 @@ class Role(db.Model):
     __tablename__ = "role"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), unique=True)
-
+    users = db.relationship('User', backref='role',
+                                lazy='dynamic')
 
     def __init__(self, title):
         self.title = title
