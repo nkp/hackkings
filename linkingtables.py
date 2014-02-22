4db2cb0c4bcf657deaee9c130b90a3928e3f61b2
Sachin Pande <sazap10@gmail.com>
2014-02-22 17:54:03 +0000

    addef links from user-skills and project-skills

--- linkingtables.py
+++ linkingtables.py
@@ -2,3 +2,13 @@ userprojectlink = db.Table('userprojectlink',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('project_id', db.Integer, db.ForeignKey('project.id'))
 )
+
+skill_projects_link = db.Table('skill_project_link',
+    db.Column('skill_id', db.Integer, db.ForeignKey('skill.id')),
+    db.Column('project_id', db.Integer, db.ForeignKey('project.id'))
+)
+
+skill_users_link = db.Table('skill_user_link',
+    db.Column('skill_id', db.Integer, db.ForeignKey('skill.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+)
