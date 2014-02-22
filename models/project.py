0dd7afbd976b5ba60a6c81c18c362c413150b6dc
Sachin Pande <sazap10@gmail.com>
2014-02-22 19:18:45 +0000

    added missing fields in project

--- models/project.py
+++ models/project.py
@@ -1,18 +1,20 @@
+from hackkings.models.attachment import Attachment
 from hackkings import db
 
 class Project(db.Model):
     __tablename__ = "project"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
-    #developers
-    #completed
-    #state
-    #proposer
-    #description
-    #skills_needed
-    #time_estimate
-    #difficulty
-    #attachments
+    completed = db.Column(db.Boolean, unique=False)
+    #developers is back reffed
+    state = db.Column(db.Integer, db.ForeignKey('state.id'))
+    proposer = db.Column(db.Integer, db.ForeignKey('user.id'))
+    description = db.Column(db.Text, unique=False)
+    #skills are back reffed
+    #time in hours
+    time_estimate = db.Column(db.Integer, unique=False)
+    difficulty = db.Column(db.Integer, unique=False)
+    attachments = db.relationship('Attachment', backref='project', lazy='dynamic')
 
     def __init__(self, name,developers,completed,state,proposer,description,skills_needed,time_estimate,difficulty,attachments):
         self.name = name
