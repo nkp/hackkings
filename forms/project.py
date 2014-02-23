44800adf36c38e817296e9f8be3ee047937cb7af
Sachin Pande <sazap10@gmail.com>
2014-02-23 01:40:29 +0000

    added project registration form

---
+++ forms/project.py
@@ -0,0 +1,9 @@
+from flask_wtf import Form
+from wtforms import TextField, PasswordField, RadioField
+from wtforms.validators import DataRequired
+
+class ProjectForm(Form):
+    name = TextField('name', validators=[DataRequired()])
+    description = TextField('description', validators=[DataRequired()])
+    time_estimate = TextField('time_estimate', validators=[DataRequired()])
+    difficulty = TextField('difficulty', validators=[DataRequired()])
