449f8cba81f06b0476d407f60fc1ffd16165520b
nkp <nicprettejohn@gmail.com>
2014-02-23 06:02:05 +0000

    Message time info

--- models/message.py
+++ models/message.py
@@ -1,23 +1,24 @@
 from hackkings import db
 from hackkings.models import MessageThread
 
+from datetime import datetime
+
 class Message(db.Model):
     __tablename__ = "message"
     id = db.Column(db.Integer, primary_key=True)
     thread_id = db.Column(db.Integer, db.ForeignKey('message_thread.id'))
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    time = db.Column(db.DateTime)
+    time = db.Column(db.DateTime, default=lambda ctx: datetime.utcnow())
     message = db.Column(db.Text)
 
     def __init__(self, thread, sender, message, time):
         self.thread = thread
         self.sender = sender
         self.message = message
-        self.time = time
 
     def __repr__(self):
         return '<Message %r>' % self.message
 
-    def validate__content(content):
+    def validate_content(content):
         if len(content) > 5000:
             raise ValidationError('To be implemented')
