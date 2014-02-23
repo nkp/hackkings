38ac3305f69bc4a946a4b88092077299b2bc3fbc
southrop <andyli92@gmail.com>
2014-02-23 05:48:03 +0000

    Landing page for logged in

--- views/index.py
+++ views/index.py
@@ -1,8 +1,10 @@
 from flask import render_template, current_app
-
+from flask_login import current_user
 from hackkings import app
 
 @app.route('/')
 def index():
+    if current_user.is_authenticated():
+        return render_template('landing.html')
     return render_template('index.html')
 
