b266b9ae525fa1d00eb946968771fa7da69b1338
southrop <andyli92@gmail.com>
2014-02-23 10:46:24 +0000

    Cleaned up dummydata

--- dummydata.py
+++ dummydata.py
@@ -2,24 +2,31 @@ from hackkings import db
 from hackkings.constants import ROLES, STATES
 from hackkings.models import User,Skill,Project
 
+# Create users
 sachin = User.create("sazap10", "sazap10@gmail.com", "password", ROLES.PROPOSER)
 andrew = User.create("southrop", "southrop113@gmail.com", "hunter2", ROLES.DEVELOPER)
 nic = User.create("nic", "nick@gmail.com", "password", ROLES.DEVELOPER)
 Ilija= User.create("Ilija", "Ilija@gmail.com", "letmein", ROLES.DEVELOPER)
-someSkills = ["java", "c++","js", "c", "python", "web dev"]
+
+# Create Skills
+someSkills = ["Java", "C++","JavaScript", "C", "Python", "Web Dev"]
 skillObjects = map(Skill,someSkills);
 map(db.session.add, skillObjects)
 db.session.commit()
+
+# Add skills to users
 map(andrew.add_skill, skillObjects)
 map(nic.add_skill, skillObjects)
 map(Ilija.add_skill, skillObjects)
-names = ["project", "awesome project", "even awesomer project", "super awesome project","super duper awesomer project"]
+
+# Create Projects
+names = ["Project", "Awesome Project", "Even Awesomer Project", "Super Awesome Project", "Super Duper Awesomer Project"]
 for name in names:
-    project = Project(name, sachin, "some awesome project description", "9000", "0")
+    project = Project(name, sachin, "An awesome project description", "9000", "0")
     project.add_skill(skillObjects[1])
-    if name == "project":
+    if name == "Project":
     	project.add_developer(andrew)
-    if name == "awesome project":
+    if name == "Even Awesomer Project":
     	project.add_developer(andrew)
     	project.set_complete()
     db.session.add(project)
