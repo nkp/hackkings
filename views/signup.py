87831383ee086e6656272530de7df5b4fd182298
nkp <nicprettejohn@gmail.com>
2014-02-23 06:00:18 +0000

    Signup fixes

--- views/signup.py
+++ views/signup.py
@@ -13,7 +13,8 @@ def signup(role=None):
         return redirect('/')
     print 'signup'
     signup_form = SignupForm()
-    signup_form.role.choices = [(ROLES.DEVELOPER, 'Developer'), (ROLES.PROPOSER, 'Proposer')]
+    signup_form.role.choices = [(ROLES.DEVELOPER, 'I\'m a developer'), (ROLES.PROPOSER, 'I\'m a proposer!')]
+    print dir(signup_form.role)
 
     if signup_form.validate_on_submit():
         User.create(signup_form.username.data, signup_form.email.data, 
@@ -23,6 +24,8 @@ def signup(role=None):
         return render_template('signup.html', signup_form = signup_form)
     role = role.lower().strip()
     if role == 'developer':
+        signup_form.role.data = ROLES.DEVELOPER
         return render_template('signup.developer.html', signup_form = signup_form)
     elif role == 'proposer':
+        signup_form.role.data = ROLES.PROPOSER
         return render_template('signup.proposer.html', signup_form = signup_form)
