341b469327cbe646e1241a49a7284f0398a0ecd5
Sachin Pande <sazap10@gmail.com>
2014-02-23 12:31:00 +0000

    added labels to project signup

--- forms/project.py
+++ forms/project.py
@@ -3,8 +3,8 @@ from wtforms import TextField, SubmitField
 from wtforms.validators import DataRequired
 
 class ProjectForm(Form):
-    name = TextField('name', validators=[DataRequired()])
-    description = TextField('description', validators=[DataRequired()])
-    time_estimate = TextField('time_estimate', validators=[DataRequired()])
-    difficulty = TextField('difficulty', validators=[DataRequired()])
+    name = TextField('Name', validators=[DataRequired()])
+    description = TextField('Description', validators=[DataRequired()])
+    time_estimate = TextField('Estimated Time', validators=[DataRequired()])
+    difficulty = TextField('Difficulty', validators=[DataRequired()])
     submit = SubmitField()
