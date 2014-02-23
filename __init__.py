a0962b2420a48509cf03c0be59f4c4096300f618
nkp <nicprettejohn@gmail.com>
2014-02-23 04:46:12 +0000

    Modernize flask library import

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from hackkings.constants import SITENAME
 from flask_login import LoginManager
 
